2d3aad15181113be6f825c13e123a7c993a3cc70
hee <heheeun13@gmail.com>
2024-10-12 16:19:38 +0900

    Feat: 2주차 수업 실습 - Delegate, Closer* 실습 자료

---
+++ 35-Seminar-hee2/35-Seminar-hee2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  35-Seminar-hee2
+//
+//  Created by 김희은 on 10/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
