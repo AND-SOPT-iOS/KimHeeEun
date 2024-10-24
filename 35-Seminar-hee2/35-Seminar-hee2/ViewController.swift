b91b07084ebfa31d91b82397ecc2945fcea034f4
hee <heheeun13@gmail.com>
2024-10-25 00:40:19 +0900

    Feat: 초기 세팅

--- 35-Seminar-hee2/35-Seminar-hee2/ViewController.swift
+++ 35-Seminar-hee2/35-Seminar-hee2/ViewController.swift
@@ -6,14 +6,59 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
 
+    private let scrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        scrollView.backgroundColor = .systemBackground
+        scrollView.showsVerticalScrollIndicator = true
+        return scrollView
+    }()
+    
+    private var contentView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setStyle()
+        setUI()
+        setLayout()
     }
 
-
+    private func setStyle() {
+        self.view.backgroundColor = .systemBackground
+    }
+    
+    private func setUI() {
+        [scrollView, contentView,].forEach {
+            $0.translatesAutoresizingMaskIntoConstraints = false
+            self.view.addSubview($0)
+        }
+    }
+    
+    private func setLayout() {
+        scrollView.snp.makeConstraints {
+            $0.top.equalToSuperview()
+            $0.bottom.equalToSuperview()
+            $0.leading.equalToSuperview()
+            $0.trailing.equalToSuperview()
+        }
+        
+        contentView.snp.makeConstraints {
+            $0.top.equalTo(scrollView.snp.top)
+            $0.leading.equalTo(scrollView.snp.leading)
+            $0.trailing.equalTo(scrollView.snp.trailing)
+            $0.height.equalTo(100)
+            
+        }
+    }
+    
 }
 
