fe13b5a05d0b3c18c64011131d6ba9171847be0a
hee <heheeun13@gmail.com>
2024-12-14 03:45:33 +0900

    Refactor: StoryCell Refactor

--- 35-Seminar-week7/35-Seminar-week7/Cell/StoryCell.swift
+++ 35-Seminar-week7/35-Seminar-week7/Cell/StoryCell.swift
@@ -11,22 +11,28 @@ struct StroyCell: View {
     let storyData: StoryUserData
     
     var body: some View {
-        var strokeColor = storyData.storyCategory == .all ? Color.purple : Color.green
-        
         VStack {
             ZStack {
-                storyData.userProfileImage
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 80, height: 80)
-                    .clipShape(Circle())
-                Circle()
-                    .stroke(strokeColor, lineWidth: 5) // 테두리
-                    .frame(width: 90, height: 90)
-                    .foregroundColor(Color.clear)
+                userImage
+                circleStroke
             }
             Text(storyData.userID)
         }
+    }
+        
+    private var userImage: some View {
+        storyData.userProfileImage
+            .resizable()
+            .scaledToFit()
+            .frame(width: 80, height: 80)
+            .clipShape(Circle())
+    }
+    
+    private var circleStroke: some View {
+        let strokeColor = self.storyData.storyCategory == .all ? Color.purple : Color.green
+        return Circle()
+            .stroke(strokeColor, lineWidth: 5) // 테두리
+            .frame(width: 90, height: 90)
     }
 }
 
